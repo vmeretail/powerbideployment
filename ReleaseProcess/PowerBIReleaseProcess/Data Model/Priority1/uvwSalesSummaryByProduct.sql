d05c2e6427fb718bfc2dd4886bedc004e2d9d1c2
David Spence <david.spence@vmeretail.com>
2021-11-08 16:53:30 +0000

    updating view and SP

--- ReleaseProcess/PowerBIReleaseProcess/Data Model/Priority1/uvwSalesSummaryByProduct.sql
+++ ReleaseProcess/PowerBIReleaseProcess/Data Model/Priority1/uvwSalesSummaryByProduct.sql
@@ -1,26 +1,29 @@
 CREATE OR ALTER VIEW uvwSalesSummaryByProduct
 AS
-SELECT 
-			[AggregateId]
-           ,[StoreId]
-           ,[CompletedDate]
-           ,[Department]
-           ,[Section]
-           ,[Subsection]
-           ,[ProductDescription]
-           ,[TaxRate]
-           ,[PaymentType]
-           ,[Reason Code Type]
-           ,[TransactionStatus]
-           ,[DiscountType]
-           ,[Tax Rate Count]
-           ,[SALE exc VAT]
-           ,[SALE inc VAT]
-           ,[Margin £]
-           ,[YearWeekNumber]
-           ,[Year]
-           ,[Week]
-           ,[DayOfWeekShort]
-           ,[DayOfWeek]
-		   ,[Store]
-FROM SalesSummaryByProduct
+    SELECT 
+	    [AggregateId]
+        ,store.[StoreId]
+        ,[CompletedDate]
+        ,[DepartmentName] [Department]
+        ,[SectionName] [Section]
+        ,[SubsectionName] [Subsection]
+        ,[Description] [ProductDescription]
+        ,[TaxRate]
+        ,[PaymentType]
+        ,[Reason Code Type]
+        ,[TransactionStatus]
+        ,[DiscountType]
+        ,[Tax Rate Count]
+        ,[SALE exc VAT]
+        ,[SALE inc VAT]
+        ,[Margin £]
+        ,[YearWeekNumber]
+        ,[Year]
+        ,[Week]
+        ,[DayOfWeekShort]
+        ,[DayOfWeek]
+	    ,store.[Name] [store] 
+    FROM SalesSummaryByProductTest
+    INNER JOIN producthierarchy ON SalesSummaryByProductTest.subSectionId = producthierarchy.subSectionId
+    INNER JOIN organisationproduct ON SalesSummaryByProductTest.OrganisationProductId = organisationproduct.OrganisationProductId
+    INNER JOIN store ON SalesSummaryByProductTest.storeId = store.StoreId
