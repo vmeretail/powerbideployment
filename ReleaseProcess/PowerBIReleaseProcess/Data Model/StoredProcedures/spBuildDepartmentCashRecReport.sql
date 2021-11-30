3a8607453028a30b9e741916f3df71e8c9d1b473
David Spence <david.spence@vmeretail.com>
2021-11-30 22:57:12 +0000

    wip

--- ReleaseProcess/PowerBIReleaseProcess/Data Model/StoredProcedures/spBuildDepartmentCashRecReport.sql
+++ ReleaseProcess/PowerBIReleaseProcess/Data Model/StoredProcedures/spBuildDepartmentCashRecReport.sql
@@ -1,71 +1,153 @@
 CREATE OR ALTER   PROCEDURE [dbo].[spBuildDepartmentCashRecReport] @reportStartDate datetime, @DepartmentId uniqueidentifier
 AS
-	delete from CashRecReporting WHERE ReportDate >= @reportStartDate and DepartmentId = @DepartmentId
+	DELETE FROM CashRecReportingTEST WHERE ReportDate >= @reportStartDate
 
-	insert into CashRecReporting(
+	INSERT INTO CashRecReportingTEST
+	(
 		ReportDate, 
-		StoreId,
 		DepartmentId, 
+		StoreId,
 		basketCount,
 		basketTotal,
 		saleType,
-		marginTotal)
+		marginTotal
+	)
 
 	-- total sales
-	select salestransactioncompleted.CompletedDate, salestransactioncompleted.StoreId, @DepartmentId, count(distinct salestransactioncompleted.aggregateId), sum(salestransactioncompleted.baskettotal), 'total', sum(salestransactioncompleted.MarginValue)
-	from 
-	(	
-		select distinct aggregateId
-		from ##salesInReportDate
-	) as result
-	inner join salestransactioncompleted on salestransactioncompleted.AggregateId = result.AggregateId
-	group by salestransactioncompleted.CompletedDate, salestransactioncompleted.StoreId
+	SELECT 
+		salestransactioncompleted.CompletedDate, 
+		departmentId, 
+		salestransactioncompleted.StoreId, 
+		COUNT(DISTINCT salestransactioncompleted.aggregateId), 
+		SUM(salestransactioncompleted.baskettotal), 
+		'total', 
+		SUM(salestransactioncompleted.MarginValue)
+	FROM 
+	(
+		SELECT DISTINCT departmentId from producthierarchy
+	) department
+	INNER JOIN salestransactioncompleted On 1 = 1
+	WHERE salestransactioncompleted.CompletedDate >= @reportStartDate
+	GROUP BY salestransactioncompleted.CompletedDate, salestransactioncompleted.StoreId, departmentId
 
-	union all 
+	UNION ALL
 
 	-- sales with only the interesting department
-	select salestransactioncompleted.CompletedDate, salestransactioncompleted.StoreId, @DepartmentId, count(*), sum(salestransactioncompleted.baskettotal), 'onlyselecteddepartment', sum(salestransactioncompleted.MarginValue)
-	from 
+	SELECT 
+		salestransactioncompleted.CompletedDate, 
+		departmentId, 
+		salestransactioncompleted.StoreId, 
+		count(*), 
+		SUM(salestransactioncompleted.baskettotal), 
+		'onlyselecteddepartment', 
+		SUM(salestransactioncompleted.MarginValue)
+	FROM salestransactioncompleted
+	INNER JOIN
 	(
-		select distinct salestransactionline.aggregateId 
-		from salestransactionline
-		where salestransactionline.aggregateId IN (select distinct ##salesInReportDate.aggregateId
-		from ##salesInReportDate
-		where departmentId = @departmentid)	
-		group by salestransactionline.aggregateId
-		having count(distinct salestransactionline.departmentId) = 1
-	) as result
-	inner join salestransactioncompleted on salestransactioncompleted.AggregateId = result.AggregateId
-	group by salestransactioncompleted.CompletedDate, salestransactioncompleted.StoreId		
+		SELECT DISTINCT a.aggregateId, departmentId 
+		FROM 
+		(
+			SELECT aggregateId 
+			FROM salestransactionline
+			GROUP BY aggregateId
+			HAVING COUNT(DISTINCT departmentId) = 1
+		) a
+		INNER JOIN  
+		(
+			SELECT aggregateId, departmentId
+			FROM salestransactionline
+		) b ON a.AggregateId = b.AggregateId
+	) result ON salestransactioncompleted.AggregateId = result.AggregateId
+	WHERE salestransactioncompleted.CompletedDate >= @reportStartDate AND departmentId IS NOT NULL
+	GROUP BY salestransactioncompleted.CompletedDate, salestransactioncompleted.StoreId, departmentId
 
-	union all
+	UNION ALL
 
 	-- sales with the interesting department but others
-	select salestransactioncompleted.CompletedDate, salestransactioncompleted.StoreId, @DepartmentId, count(*), sum(salestransactioncompleted.baskettotal), 'includingselecteddepartment', sum(salestransactioncompleted.MarginValue)
-	from 
+	SELECT 
+		salestransactioncompleted.CompletedDate, 
+		departmentId, 
+		salestransactioncompleted.StoreId, 
+		count(*), 
+		SUM(salestransactioncompleted.baskettotal), 
+		'includingselecteddepartment', 
+		SUM(salestransactioncompleted.MarginValue)
+	FROM salestransactioncompleted
+	INNER JOIN
 	(
-		select distinct salestransactionline.aggregateId 
-		from salestransactionline
-		where salestransactionline.aggregateId IN (select distinct ##salesInReportDate.aggregateId
-		from ##salesInReportDate
-		where departmentId = @departmentid)	
-		group by salestransactionline.aggregateId
-		having count(distinct salestransactionline.departmentId) > 1
-	) as result
-	inner join salestransactioncompleted on salestransactioncompleted.AggregateId = result.AggregateId
-	group by salestransactioncompleted.CompletedDate, salestransactioncompleted.StoreId
-	
-	union all
+		SELECT DISTINCT a.aggregateId, departmentId 
+		FROM 
+		(
+			SELECT aggregateId 
+			FROM salestransactionline
+			GROUP BY aggregateId
+			HAVING COUNT(DISTINCT departmentId) > 1
+		) a
+		INNER JOIN  
+		(
+			SELECT aggregateId, departmentId
+			FROM salestransactionline
+		) b ON a.AggregateId = b.AggregateId
+	) result ON salestransactioncompleted.AggregateId = result.AggregateId
+	WHERE salestransactioncompleted.CompletedDate >= @reportStartDate AND departmentId IS NOT NULL
+	GROUP BY salestransactioncompleted.CompletedDate, salestransactioncompleted.StoreId, departmentId
 
-	select salestransactioncompleted.CompletedDate, salestransactioncompleted.StoreId, @DepartmentId, count(*), sum(salestransactioncompleted.baskettotal), 'excludingselecteddepartment', sum(salestransactioncompleted.MarginValue)
-	from 
-	(
-		select distinct salestransactionline.aggregateId 
-		from salestransactionline
-		inner join ##salesInReportDate on ##salesInReportDate.aggregateId = salestransactionline.aggregateId
-		where salestransactionline.aggregateId NOT IN (select distinct ##salesInReportDate.aggregateId
-		from ##salesInReportDate
-		where departmentId = @departmentid)
-	) as result
-	inner join salestransactioncompleted on salestransactioncompleted.AggregateId = result.AggregateId
-	group by salestransactioncompleted.CompletedDate, salestransactioncompleted.StoreId
+	DECLARE @DepartmentId UNIQUEIDENTIFIER  
+	DECLARE db_cursor CURSOR FOR 
+		SELECT DISTINCT [DepartmentId]
+		FROM [dbo].[producthierarchy]
+
+	BEGIN TRY
+		DROP TABLE ##salesInReportDate
+	END TRY
+	BEGIN CATCH
+	  --IGNORE EXCEPTION IF TABLE DOES NOT EXIST
+	END CATCH
+	
+	SELECT salestransactionline.aggregateId, salestransactionline.departmentId
+	INTO ##salesInReportDate
+	FROM salestransactionline
+	INNER JOIN salestransactioncompleted ON salestransactioncompleted.AggregateId = salestransactionline.aggregateId
+	WHERE salestransactioncompleted.CompletedDate >= @reportStartDate
+
+	OPEN db_cursor  
+	FETCH NEXT FROM db_cursor INTO @DepartmentId  
+
+	WHILE @@FETCH_STATUS = 0  
+	BEGIN 
+		INSERT INTO CashRecReportingTEST
+		(
+			ReportDate, 
+			DepartmentId, 
+			StoreId,
+			basketCount,
+			basketTotal,
+			saleType,
+			marginTotal
+		)
+
+		SELECT 
+			salestransactioncompleted.CompletedDate, 
+			@DepartmentId, 
+			salestransactioncompleted.StoreId, 
+			COUNT(*), 
+			SUM(salestransactioncompleted.baskettotal), 
+			'excludingselecteddepartment', 
+			SUM(salestransactioncompleted.MarginValue)
+		FROM 
+		(
+			SELECT DISTINCT salestransactionline.aggregateId 
+			FROM salestransactionline
+			INNER JOIN ##salesInReportDate on ##salesInReportDate.aggregateId = salestransactionline.aggregateId
+			WHERE salestransactionline.aggregateId NOT IN (SELECT DISTINCT ##salesInReportDate.aggregateId
+			FROM ##salesInReportDate
+			WHERE departmentId = @DepartmentId)
+		) AS result
+		INNER JOIN salestransactioncompleted ON salestransactioncompleted.AggregateId = result.AggregateId
+		group by salestransactioncompleted.CompletedDate, salestransactioncompleted.StoreId	
+    
+		FETCH NEXT FROM db_cursor INTO @DepartmentId	  
+	END 
+
+	CLOSE db_cursor
+	DEALLOCATE db_cursor
