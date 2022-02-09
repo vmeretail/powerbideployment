36a57d20809c77c4d95fecb40fb92d1c626912ed
StuartFergusonVme <stuart.ferguson@vmeretail.com>
2022-02-09 12:49:57 +0000

    Add Area manager and update dataset/reports

---
+++ ReleaseProcess/PowerBIReleaseProcess/Data Model/Priority1/uvwAreaManager.sql
@@ -0,0 +1,12 @@
+CREATE or ALTER VIEW [dbo].[uvwAreaManager]
+AS
+select NULL as 'AreaManagerId',
+	   'None' as 'Name'
+
+union all
+
+select areamanager.AreaManagerId,
+	   areamanager.Name
+from areamanager
+
+GO
