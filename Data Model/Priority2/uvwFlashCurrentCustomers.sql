eb5cf60490014aba3257757b848de5e527d94387
Stuart Ferguson <stuart.ferguson@vme.coop>
2022-05-09 12:37:43 +0100

    :|

--- Data Model/Priority2/uvwFlashCurrentCustomers.sql
+++ Data Model/Priority2/uvwFlashCurrentCustomers.sql
@@ -2,12 +2,12 @@ CREATE OR ALTER VIEW [dbo].[uvwFlashCurrentCustomers]
 AS
 SELECT 
 	CompletedDate,
-	StoreReportingId,
+	salestransactioncompleted.StoreReportingId,
 	COUNT(aggregateid) [Customer Count]
 FROM salestransactioncompleted
 INNER JOIN uvwStoresView ON salestransactioncompleted.StoreId = uvwStoresView.StoreId
 WHERE salestransactioncompleted.BasketTotal != 0
 GROUP BY
 	CompletedDate,
-	StoreReportingId
+	salestransactioncompleted.StoreReportingId
 
